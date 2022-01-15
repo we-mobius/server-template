19f977ff5340d76291d6625b14873e3a53ac0c76
kongxiangyan <kcigaret@outlook.com>
2022-01-15 19:36:49 +0800

    chore: add dotenv

--- bin/index.js
+++ bin/index.js
@@ -1,5 +1,5 @@
 import('./_index.cjs').then(pkg => {
   const { starter } = pkg.default.MobiusServer
-  console.log('Starting Mobius Server...')
+  console.log(`Starting Mobius Server: ${process.env.PROJECT_NAME}...`)
   starter({})
 })
