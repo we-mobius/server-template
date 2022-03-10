22fdd7e53823a3c26e15de9709eabff1635a5fe6
unknown <kcigaret@outlook.com>
2022-03-11 00:50:49 +0800

    feat: use apolloServerDriver

--- src/ts/server/serve/v1/graphql/index.ts
+++ src/ts/server/serve/v1/graphql/index.ts
@@ -1,5 +1,8 @@
 import { replayWithLatest } from 'MobiusUtils'
 import { mainClientDriverInstance, clientUsedAppRouteDriverInstance } from 'FreeServices/client'
+import { apolloServerDriver } from './apollo'
+
+apolloServerDriver.inputs.start.mutate(() => true)
 
 const { inputs: { response }, outputs: { currentRoute } } = mainClientDriverInstance
 const { contexts: { routeProcessT } } = clientUsedAppRouteDriverInstance
